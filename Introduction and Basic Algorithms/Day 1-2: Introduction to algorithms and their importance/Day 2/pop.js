410e582080bcc86e8396c960ee64b7c32452e484
Daniel Williams <williamsoligodaniel@gmail.com>
2023-08-28 17:33:44 +0100

    Update pop.js

--- Introduction and Basic Algorithms/Day 1-2: Introduction to algorithms and their importance/Day 2/pop.js
+++ Introduction and Basic Algorithms/Day 1-2: Introduction to algorithms and their importance/Day 2/pop.js
@@ -11,3 +11,20 @@ test('removes the last element from the array', () => {
   
   expect(cities).toEqual(expectedArray); // Check if the array matches the expected state
 });
+
+
+/*The distinction between toEqual and toBe is important when it comes to Jest testing.
+
+toBe: This matcher is used for primitive data types like numbers, strings, and booleans. It performs a strict equality check using 
+the === operator. It checks if two values are the same instance in memory as well as having the same value. However, for arrays
+and objects, using toBe might not give you the expected results, as it checks for reference equality, not structural equality.
+
+toEqual: This matcher is used for comparing objects, arrays, and other complex data structures. It performs a deep equality
+check, which means it compares the values of the properties within objects and arrays, rather than checking if they're the same 
+instance in memory.
+
+In your specific test case, you're testing whether the array after calling cities.pop() matches the expected array with the last 
+element removed. Since you're comparing arrays (which are objects) and not primitive values, using toBe won't work as expected, because it will check for reference equality, not whether their contents are equal.
+
+So, when comparing arrays, objects, or other complex data structures, it's more appropriate to use toEqual because it performs a 
+deep equality check. In contrast, toBe is better suited for simple primitive values like strings, numbers, and booleans.*/
