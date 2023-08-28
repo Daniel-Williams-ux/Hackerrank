e1985c5a55d5d2cc7a6e3d21d452d16f9cce89ad
Daniel Williams <williamsoligodaniel@gmail.com>
2023-08-28 16:50:37 +0100

    Update access.js

--- Introduction and Basic Algorithms/Day 1-2: Introduction to algorithms and their importance/Day 2/access.js
+++ Introduction and Basic Algorithms/Day 1-2: Introduction to algorithms and their importance/Day 2/access.js
@@ -4,3 +4,31 @@ function boxes(arr) {
    return arr[0]
 }
 module.exports = boxes;
+
+
+//test
+const boxes = require('./access');
+describe('boxes function', () => {
+  test('accesses the first element in the array', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const result = boxes(numbers);
+    expect(result).toBe(1)
+  })
+})
+
+
+const boxes = require('./path-to-your-function'); // Adjust the path accordingly
+
+describe('boxes function', () => {
+    test('accesses the first element of the array', () => {
+        const numbers = [1, 2, 3, 4, 5];
+        const result = boxes(numbers);
+        expect(result).toBe(1);
+    });
+
+    test('accesses the first element of the first inner array', () => {
+        const nestedNumbers = [[1, 2], [3, 4], [5, 6]];
+        const result = boxes(nestedNumbers);
+        expect(result).toBe(1);
+    });
+});
