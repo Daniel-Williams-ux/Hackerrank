cc60456e49b6078d1b08fce493f14ac9be395480
Daniel Williams <williamsoligodaniel@gmail.com>
2023-08-28 17:59:03 +0100

    Create Accessing_every_item.js

---
+++ Introduction and Basic Algorithms/Day 1-2: Introduction to algorithms and their importance/Day 2/Accessing_every_item.js
@@ -0,0 +1,6 @@
+//Very often you will want to access every item in the array. You can do this using the for...of statement:
+const birds = ["Parrot", "Falcon", "Owl"];
+
+for (const bird of birds) {
+  console.log(bird);
+}
