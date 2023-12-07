5fe94f0c6cd83ea3190b2c3562d5a37a0e5b66f6
Daniel Williams <williamsoligodaniel@gmail.com>
2023-12-07 13:46:02 +0100

    Update hashsetdupplicate.js

--- Hash Tables/hashsetdupplicate.js
+++ Hash Tables/hashsetdupplicate.js
@@ -22,12 +22,25 @@ Output: true
  * @param {number[]} nums
  * @return {boolean}
  */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var containsDuplicate = function(nums) {
-    const hashset = new Set();
-    for (num of nums) {
-        if (hashset.has(num)) {
-            return true
+    const hashSet = new Set();
+    
+    for (const num of nums) {
+        if (hashSet.has(num)) {
+            return true; // Duplicate found
         }
+        hashSet.add(num);
     }
-    return false
+    
+    return false; // No duplicates found
 };
+
+// Example usage:
+console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
+
